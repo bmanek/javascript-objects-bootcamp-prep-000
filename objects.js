08b37f8acfd3e551f3e871c486ede609cf0df0d8
Ben Manekin <ben.manekin@gmail.com>
2019-01-11 19:21:31 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
-var playlist = {"Nine Inch Nails" : "Closer"};
+var playlist = {"Nine Inch Nails" : "Closer", "Slowdive" : "Alison", "My Bloody Valentine" : ""};
 
 function updatePlaylist(playlist, artistName, songTitle) {
   Object.assign({}, playlist, {[artistName] : songTitle});
