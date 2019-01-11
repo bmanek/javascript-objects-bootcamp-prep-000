9b8a2af7355eda8c0d19d704904a1da4883cbb0f
Ben Manekin <ben.manekin@gmail.com>
2019-01-11 19:35:43 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,9 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName) {
-  delete playlist.artistName;
+  delete playlist[artistName];
   return playlist;
 }
-
-removeFromPlaylist(playlist, 'Nine Inch Nails')
-console.log(playlist)
