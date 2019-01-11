8d9fd0968960507b7a27b515451f518d79d0c80c
Ben Manekin <ben.manekin@gmail.com>
2019-01-11 18:56:16 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,6 @@
 var playlist = { 'artist name' : 'song title' }
 
 function updatePlaylist(obj, name, song) {
-  return Object.assign({}, obj, {[name] : song})
+  return Object.assign({}, obj, {[name] : song});
+
 }
